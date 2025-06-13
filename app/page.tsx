a56602e9c44022cf1bb6454a8486bcab5b133a9e
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-06-13 13:32:14 +0300

    set up + homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+import { Button, Flex, Heading } from "@radix-ui/themes";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      justify="center"
+      gap="6"
+      minHeight="100vh"
+    >
+      <Heading size="8">DotCode Test Task</Heading>
+      <Flex direction="row" align="center" gap="8">
+        <Button asChild className="!cursor-pointer" variant="solid">
+          <Link href="/workspace">Go to Workspace</Link>
+        </Button>
+        <Button asChild className="!cursor-pointer" variant="solid">
+          <Link href="/transactions">Go to Transactions</Link>
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
